57b41c61987fe9cf826eec57fb308884fb9d0edd
EricWu <43841531+wl00254294@users.noreply.github.com>
2019-07-08 12:03:28 +0800

    Update SpringAopApplication.java

--- src/main/java/com/example/demo/SpringAopApplication.java
+++ src/main/java/com/example/demo/SpringAopApplication.java
@@ -11,7 +11,7 @@ public class SpringAopApplication {
 	public static void main(String[] args) {
 		
 		//System.out.println("2019-07-08") new branch;
-		//fetch
+		//fetch from 20190708
 		SpringApplication.run(SpringAopApplication.class, args);
 	}
 
